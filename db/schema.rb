f8c5dcfc2302865d5b6b45b76f68c1be9aba655d
TakPohui <takpohuieslichestno@mail.ru>
2020-10-24 17:29:45 +0300

    Create model and migration tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_004812) do
+ActiveRecord::Schema.define(version: 2020_10_24_142458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2020_10_24_004812) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["organization_id"], name: "index_registered_members_on_organization_id"
     t.index ["user_id"], name: "index_registered_members_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "unregistered_members", force: :cascade do |t|
