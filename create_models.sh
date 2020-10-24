a29062a60bf04e922b19f65f73d3098ba1bd887d
TakPohui <takpohuieslichestno@mail.ru>
2020-10-24 03:54:45 +0300

    Add generate script creating unregistered member model

--- create_models.sh
+++ create_models.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # rails g model Organization name:string description:string approve_status:integer -force
-rails generate model RegisteredMember member_role:integer organization:references user:references
+# rails generate model RegisteredMember member_role:integer organization:references user:references -force
+# rails generate model UnregisteredMember member_role:integer organization:references code:string email:string
