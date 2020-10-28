7a530b0829f860bceb94914e5ba7341f88ddb15c
TakPohui <takpohuieslichestno@mail.ru>
2020-10-28 17:52:28 +0300

    Add associations for registered member serializer

--- app/serializers/registered_member_serializer.rb
+++ app/serializers/registered_member_serializer.rb
@@ -1,3 +1,8 @@
 class RegisteredMemberSerializer < ActiveModel::Serializer
-  attributes :id, :user, :organization, :member_role
+  attributes :id,
+             :user,
+             :organization,
+             :member_role
+
+  belongs_to :organization
 end
