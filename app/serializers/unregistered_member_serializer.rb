52ecfa9fea40f579a5e3e967ed75408c6bbae78e
TakPohui <takpohuieslichestno@mail.ru>
2020-10-28 17:52:40 +0300

    Add associations for unregistered member serializer

--- app/serializers/unregistered_member_serializer.rb
+++ app/serializers/unregistered_member_serializer.rb
@@ -1,3 +1,9 @@
 class UnregisteredMemberSerializer < ActiveModel::Serializer
-  attributes :id, :organization, :member_role, :email, :code
+  attributes :id,
+             :organization,
+             :member_role,
+             :email,
+             :code
+
+  belongs_to :organization
 end
