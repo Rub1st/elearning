48c9716f51016f076399b4415cbb937605e958f8
TakPohui <takpohuieslichestno@mail.ru>
2020-10-28 17:38:43 +0300

    Add associations for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :login, :email, :birthday, :user_role, :avatar
+  attributes :id,
+             :full_name,
+             :login,
+             :email,
+             :birthday,
+             :user_role,
+             :avatar
+
+  has_many :courses
+  has_many :comments
 end
