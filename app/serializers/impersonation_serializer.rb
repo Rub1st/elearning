4dd7471c6e8fa11c317abffbcdb58d4501757f75
TakPohui <takpohuieslichestno@mail.ru>
2020-10-29 13:00:44 +0300

    Add missing associations

--- app/serializers/impersonation_serializer.rb
+++ app/serializers/impersonation_serializer.rb
@@ -1,3 +1,10 @@
 class ImpersonationSerializer < ActiveModel::Serializer
-  attributes :id, :start, :end, :manager, :common
+  attributes :id,
+             :start,
+             :end,
+             :manager,
+             :common
+
+  has_one :manager
+  has_one :common
 end
