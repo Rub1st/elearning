e393dfac0015067a7431fd0aa95b5bb9d3be8db4
TakPohui <takpohuieslichestno@mail.ru>
2020-11-26 11:49:01 +0300

    Implement methods for drop repeated user answers

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,12 +1,9 @@
 class UserAnswersController < ApplicationController
-
   def create
     ua = UserAnswer.new(permit_params)
-    # temp = Answer.all.select do |element|
-    #   element.question[:id] == permit_params[:question_id] && element.value == ua.answer
-    # end
-    # p temp
-    # ua.is_correct = temp.count.positive?
+
+    old_answers_killer
+
     if ua.save
       render json: ua
     else
@@ -32,6 +29,10 @@ class UserAnswersController < ApplicationController
   end
 
   private
+
+  def old_answers_killer
+    UserAnswer.select { |item| item.user.id == permit_params[:user_id] && item.question.id == permit_params[:question_id] }.each(&:destroy)
+  end
 
   def permit_params
     params.require(:user_answer).permit(
