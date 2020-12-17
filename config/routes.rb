bea8ec215cd46778df6f8bafd080fac34cf3aa3d
TakPohui <takpohuieslichestno@mail.ru>
2020-12-17 13:01:49 +0300

    Add namespace admin with resource routes and remove unneeded routes, add path for visual routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,117 @@
 Rails.application.routes.draw do
   root 'static#index'
-  # resources :users, only: %i[create update destroy index show]                #+
 
   devise_scope :user do
-    resources :organizations, only: %i[create update destroy index show]        #+
-    resources :registered_members, only: %i[create update destroy index show]   #+
-    resources :unregistered_members, only: %i[create update destroy index show] #+
-    resources :tags, only: %i[create update index show]                         #+
-    resources :impersonations, only: %i[create update index show]
-
-    resources :courses, only: %i[create update destroy index show] do
+    resources :organizations, only: %i[create update index] do
       collection do
         get :search
       end
     end
 
-    resources :pages, only: %i[create update destroy index show]                #+
-    resources :questions, only: %i[create destroy index show]                   #+
-    resources :variants, only: %i[create destroy index show]                    #+
-    resources :answers, only: %i[create update destroy index show]              #+
-    resources :comments, only: %i[create destroy index]
-    resources :replies, only: %i[create destroy index]
-    resources :course_tags, only: %i[create index destroy]                      #+
-    resources :certificates, only: %i[create index show]
-    resources :user_courses, only: %i[create update index show]                 #+
-    resources :user_answers, only: %i[create update index show]                 #+
-    resources :reports, only: %i[create index show]                             #+
-    resources :theories, only: %i[create destroy index show]                    #+
-    resources :course_members, only: %i[create index]
+    resources :registered_members, only: %i[create destroy index]  do
+      collection do
+        get :search
+      end
+    end
+
+    resources :unregistered_members, only: %i[create destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :tags, only: %i[index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :courses, only: %i[create update index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :comments, only: %i[create destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :certificates, only: %i[create index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :user_courses, only: %i[create update index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :replies, only: %i[create destroy]
+    resources :course_tags, only: %i[create]
+    resources :impersonations, only: %i[create update]
+    resources :pages, only: %i[create update destroy index]
+    resources :questions, only: %i[create destroy index]
+    resources :variants, only: %i[create destroy]
+    resources :answers, only: %i[create update destroy index show]
+    resources :user_answers, only: %i[create]
+    resources :reports, only: %i[create index]
+    resources :theories, only: %i[create destroy index]
+    resources :course_members, only: %i[create]
+
     get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  namespace :admin do
+    resources :comments, only: %i[destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :courses, only: %i[update destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :impersonations, only: %i[index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :organizations, only: %i[update destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :tags, only: %i[create update index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :users, only: %i[update destroy index] do
+      collection do
+        get :search
+      end
+    end
+
+    resources :replies, only: %i[destroy]
+  end
+
+  resources :users, only: %i[index update] do
+    collection do
+      get :search
+    end
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
 
-  resources :users, only: %i[show] do
-  end
-
-  match '*path', to: 'static#index', via: :all
-  #   defaults: { format: :json },
-  #   path: '',
-  #   path_names: {
-  #     sign_in: 'login',
-  #     sign_out: 'logout',
-  #     registration: 'signup'
-  #   },
-  #   controllers: {
-  #     sessions: 'sessions',
-  #     registrations: 'registrations'
-  #   }
-
-  # devise_for :users, controllers: { create: 'create',
-  #                                   update: 'update',
-  #                                   destroy: 'destroy',
-  #                                   index: 'index',
-  #                                   show: 'show' }
-  # devise_for :users, path: 'auth',
-  #                    path_names: { sign_in: 'login',
-  #                                  sign_out: 'logout',
-  #                                  password: 'secret',
-  #                                  confirmation: 'verification',
-  #                                  unlock: 'unblock',
-  #                                  registration: 'register',
-  #                                  sign_up: 'cmon_let_me_in' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*path' => 'static#index', constraints: -> (req) { !(req.fullpath =~ /^\/rails\/.*/) }
 end
