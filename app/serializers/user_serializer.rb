785b85533d7b750543c4f1df176cb3bdd401e06d
TakPohui <takpohuieslichestno@mail.ru>
2020-12-24 14:52:16 +0300

    Fix nil attachment error

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,47 +7,17 @@ class UserSerializer < ActiveModel::Serializer
              :email,
              :user_role,
              :avatar_url,
-            # :courses,
              :certificate_template_url,
-            # :certificates,
              :encrypted_password,
              :decrypted_password,
              :created_at,
              :user_status
 
-  # has_many :courses
-  # has_many :comments
-  # has_many :replies
-  # has_many :certificates
-  # has_many :user_courses
-  # has_many :user_answers
-  # has_many :course_members
-
-  # def certificates
-  #   Certificate.with_attached_certificate_pdf.where(user_id: object.id)
-  # end
-
-  # def courses
-  #   Course.with_attached_image.where(author_id: object.id)
-  # end
-
-  # !
-  # def avatar_url
-  #   variant = object.avatar.variant(resize: '100x100')
-  #   rails_representation_url(variant, only_path: true)
-  # end
-
-  # !
-  # def certificate_template_url
-  #   variant = object.certificate_template.variant(resize: '200x300')
-  #   rails_representation_url(variant, only_path: true)
-  # end
-
   def avatar_url
-    url_for(object.avatar)
+    url_for(object.avatar) if object.avatar.attached?
   end
 
   def certificate_template_url
-    url_for(object.certificate_template)
+    url_for(object.certificate_template) if object.certificate_template.attached?
   end
 end
