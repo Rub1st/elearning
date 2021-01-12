82726e2a434e4bcec19def88c2d4f3751c7cd255
TakPohui <takpohuieslichestno@mail.ru>
2021-01-12 22:25:27 +0300

    Fix some syntax error

--- app/controllers/admin/replies_controller.rb
+++ app/controllers/admin/replies_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class RepliesController < ApplicationController
-    authorize!
     def destroy
+      authorize!
       Reply.find(params[:id]).destroy
       render json: Comment.all
     end
